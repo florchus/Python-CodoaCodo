d82670d98df7779335f12b8780adce9dfb5e16d5
Florencia Farias <fariasflorencia@hotmail.com>
2023-10-30 20:02:31 -0300

    Se modifica tabla clientes

--- BD/ScriptTablas.sql
+++ BD/ScriptTablas.sql
@@ -5,8 +5,8 @@ create DATABASE Librotopia
 use Librotopia
 
 create table Cliente(
-    IDCliente int AUTO_INCREMENT PRIMARY KEY,
-    Email varchar(50) not null,
+    Email varchar(50) not null PRIMARY KEY,
+    Contraseña VARCHAR(50) not null,
     Nombre varchar(50) not null,
     Apellido varchar(50) not null,
     DNI bigint not null,
@@ -14,13 +14,6 @@ create table Cliente(
     FechaDeNacimiento date not null,
     Alias varchar(60) not null,
     Estado int default 1 not null
-)
-
-create Table Login(
-    Email VARCHAR(50) not null PRIMARY KEY,
-    Contraseña VARCHAR(50) not null,
-    IDCliente int,
-    Foreign Key (IDCliente) REFERENCES Cliente(IDCliente)
 )
 
 create Table Genero(
@@ -42,32 +35,36 @@ create TABLE Libro(
 CREATE Table Comentarios (
     IDComentario int AUTO_INCREMENT PRIMARY KEY,
     IDLibro int not null,
-    IDCliente int,
+    Email VARCHAR(50),
     Comentario VARCHAR(300),
     FechaComentario date not null,
     Foreign Key (IDLibro) REFERENCES Libro(IDLibro),
-    Foreign Key (IDCliente) REFERENCES Cliente(IDCliente)
+    Foreign Key (Email) REFERENCES Cliente(Email)
 )
 
 create Table Calificaciones(
     IDCalificacion int AUTO_INCREMENT PRIMARY KEY,
     IDLibro int not null,
-    CantidadEstrellas int,
+    Estrella1 int,
+    Estrella2 int,
+    Estrella3 int,
+    Estrella4 int,
+    Estrella5 int,
     Foreign Key (IDLibro) REFERENCES Libro(IDLibro)
 )
 
 CREATE table GeneroxCliente(
     IDSeleccion int AUTO_INCREMENT PRIMARY KEY,
     IDGenero int,
-    IDCliente int,
+    Email VARCHAR(50),
     Foreign Key (IDGenero) REFERENCES Genero(IDGenero),
-    Foreign Key (IDCliente) REFERENCES Cliente(IDCliente)
+    Foreign Key (Email) REFERENCES Cliente(Email)
 )
 
 CREATE Table Favoritos(
     IDFavoritos int AUTO_INCREMENT PRIMARY KEY,
-    IDCliente int,
+    Email VARCHAR(50),
     IDLibro int,
-    Foreign Key (IDCliente) REFERENCES Cliente(IDCliente),
+    Foreign Key (Email) REFERENCES Cliente(Email),
     Foreign Key (IDLibro) REFERENCES Libro(IDLibro)
 )
