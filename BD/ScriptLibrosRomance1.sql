9a0e71d4f48fba52b3c858085ce1126a929c09e2
Christian Ezequiel Armenteros <carmenteros301122@gmail.com>
2023-12-03 15:30:42 -0300

    Script 10 libros Romance

---
+++ BD/ScriptLibrosRomance1.sql
@@ -0,0 +1,16 @@
+use Librotopia
+
+INSERT INTO libro (Titulo,Genero,Autor,Resumen,Portada)
+VALUES
+('El refugio perfecto',4,'Adriana Hartwig','','https://assets.lectulandia.com/b/ab/Adriana%20Hartwig/El%20refugio%20perfecto%20(6)/big.jpg'),
+('De Lukov, con amor',4,'Mariana Zapata','','https://assets.lectulandia.com/b/ab/Mariana%20Zapata/De%20Lukov%20con%20amor%20(1)/big.jpg'),
+('Entre tú y yo',4,'Nora Roberts','','https://assets.lectulandia.com/b/ab/Nora%20Roberts/Entre%20tu%20y%20yo%20(91)/big.jpg'),
+('El poeta sin párpados',4,'Lourdes Ventura','','https://assets.lectulandia.com/b/ab/Lourdes%20Ventura/El%20poeta%20sin%20parpados%20(1)/big.jpg'),
+('Alguien como tú',4,'Ana F. Malory','','https://assets.lectulandia.com/b/ab/Ana%20F%20Malory/Alguien%20como%20tu%20(13)/big.jpg'),
+('Prohíbeme despertar',4,'Abril Laínez','','https://assets.lectulandia.com/b/ab/Abril%20Lainez/Prohibeme%20despertar%20(3)/big.jpg'),
+('Noches en París',4,'Luis Bay','','https://assets.lectulandia.com/b/ab/Louise%20Bay/Noches%20en%20Paris%20(17)/big.jpg'),
+('La vida es breve y el deseo infinito',4,'Patrick Lapeyre','','https://assets.lectulandia.com/b/ab/Patrick%20Lapeyre/La%20vida%20es%20breve%20y%20el%20deseo%20infinito%20(1)/big.jpg'),
+('La voz que hechizó al duque',4,'Catherine Brook','','https://assets.lectulandia.com/b/ab/Catherine%20Brook/La%20voz%20que%20hechizo%20al%20duque%20(15)/big.jpg'),
+('Estaré a tú lado',4,'Ana F. Malory','','https://assets.lectulandia.com/b/ab/Ana%20F%20Malory/Estare%20a%20tu%20lado%20(14)/big.jpg'),
+
+select * from libro
