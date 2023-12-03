0832b4c1142f01532b93b40aa0a8ddf8a9456ae8
Christian Ezequiel Armenteros <carmenteros301122@gmail.com>
2023-12-03 15:53:46 -0300

    Script 10 libros Misterio

---
+++ BD/ScriptLibrosMisterio1.sql
@@ -0,0 +1,16 @@
+use Librotopia
+
+INSERT INTO libro (Titulo,Genero,Autor,Resumen,Portada)
+VALUES
+('Mayne Manor',2,'Ximo Cerdá','','https://assets.lectulandia.com/b/Ximo%20Cerda/Mayne%20Manor%20(4411)/big.jpg'),
+('La muñeca sangrienta',2,'Gastón Leroux','','https://assets.lectulandia.com/b/Gaston%20Leroux/La%20muneca%20sangrienta%20(3089)/big.jpg'),
+('Una voz en la niebla',2,'Laurent Botti','','https://assets.lectulandia.com/b/Laurent%20Botti/Una%20voz%20en%20la%20niebla%20(3671)/big.jpg'),
+('La sangre de los mártires',2,'David Hewson','','https://assets.lectulandia.com/b/David%20Hewson/La%20sangre%20de%20los%20martires%20(1)/big.jpg'),
+('Semana Santa',2,'David Hewson','','https://assets.lectulandia.com/b/David%20Hewson/Semana%20Santa%20(3)/big.jpg'),
+('Una conspiración de papel',2,'David Liss','','https://assets.lectulandia.com/b/David%20Liss/Una%20conspiracion%20de%20papel%20(3592)/big.jpg'),
+('El escalón 33',2,'Luis Zuecco','','https://assets.lectulandia.com/b/Luis%20Zueco/El%20escalon%2033%20(11)/big.jpg'),
+('La isla de los cazadores de pájaros',2,'Peter May','','https://assets.lectulandia.com/b/Peter%20May/La%20isla%20de%20los%20cazadores%20de%20pajaros%20(4924)/big.jpg'),
+('La sombra viviente',2,'Maxwell Grant','','https://assets.lectulandia.com/b/Maxwell%20Grant/La%20sombra%20viviente%20(3502)/big.jpg'),
+('La muerte viene de lejos',2,'José María Guelbenzu','','https://assets.lectulandia.com/b/Jose%20Maria%20Guelbenzu/La%20muerte%20viene%20de%20lejos%20(20)/big.jpg'),
+
+select * from libro
